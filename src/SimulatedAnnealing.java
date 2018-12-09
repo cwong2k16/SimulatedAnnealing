3a759872797e214ffa8e4f8507adc017a4accf07
cwong2015 <sirhc8002@gmail.com>
2018-12-08 21:52:00 -0500

    major speed improvements to getNeighbors after preprocessing

--- src/SimulatedAnnealing.java
+++ src/SimulatedAnnealing.java
@@ -1,11 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 
-//import java.io.IOException;
-//import javax.websocket.Session;
+import java.io.IOException;
+import javax.websocket.Session;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -14,6 +19,8 @@ import org.locationtech.jts.geom.PrecisionModel;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.geojson.GeoJsonReader;
 import org.locationtech.jts.precision.GeometryPrecisionReducer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import gerrymandering.model.District;
 import gerrymandering.model.Precinct;
@@ -34,26 +41,27 @@ public class SimulatedAnnealing implements Runnable{
 	private Random random = new Random();
 	private PrecisionModel pm = new PrecisionModel(100);
 	private GeometryPrecisionReducer reducer = new GeometryPrecisionReducer(pm);
-//	private Session session;
+	private Map<Integer,List> neighbors;
+	private Session session;
 	
     private Integer populationPrec = 100;
     private Integer compactnessPrec = 100;
     private Integer politicalPrec = 100;
 	
-    // Thread variables
+//     Thread variables
     private volatile boolean running = true;
     private volatile boolean paused = false;
     private final Object pauseLock = new Object();
 	
-	public SimulatedAnnealing(StateID stateID, Integer populationPrec, Integer compactnessPrec, Integer politicalPrec){
+	public SimulatedAnnealing(StateID stateID, Integer populationPrec, Integer compactnessPrec, Integer politicalPrec, Session session){
 		this.stateID = stateID;
 		this.populationPrec = populationPrec;
 		this.compactnessPrec = compactnessPrec;
 		this.politicalPrec = politicalPrec;
 		this.gs = new GeneratedState(stateID);
 		this.gs.copyAllData();
-		this.setupSA();
-//        this.session = session;
+		this.setupSA(stateID);
+        this.session = session;
 	}
 
 	public double acceptanceProbability(double oldCost, double newCost, double max){
@@ -111,7 +119,6 @@ public class SimulatedAnnealing implements Runnable{
 				currSol = selectBoundaryPrecinct(selectedDistrict, 
 						gs.getPrecicntList(gs.getDistrictList().get(selectedDistrictID).getDistrictId()),
 						remainingDistricts);
-				
 				if(currSol!=null){
 				
 					/* Calculate cost before making temp move */
@@ -121,12 +128,12 @@ public class SimulatedAnnealing implements Runnable{
 					populationPrec,compactnessPrec,politicalPrec);
 					
 					/* Gets list of neighbors from the selected boundary precinct */
-					List<Precinct> neighbors = getNeighbors(currSol, allPrecincts);
+					List<Integer> neighbors = getNeighbors(currSol, allPrecincts);
 					
 					/* Loops through list of neighbors and finds neighbor with different district ID */
-					for(Precinct p: neighbors) {
-						if(p.getDistrictId()!=currSol.getDistrictId()) {
-							selectedPrecinct = p;
+					for(Integer p: neighbors) {
+						if(this.gs.getPrecinct(p).getDistrictId() != currSol.getDistrictId()) {
+							selectedPrecinct = this.gs.getPrecinct(p);
 							break;
 						}
 					}
@@ -139,7 +146,7 @@ public class SimulatedAnnealing implements Runnable{
 						
 						if(ac > Math.random()){
 							move = makeMove(currSol, currSol.getDistrictId(), selectedPrecinct.getDistrictId());
-//		                    sendMove(move);
+		                    sendMove(move);
 							
 			                newFairness = ObjectiveFunction.
 			                        calculateFairnessGeneratedDistrict(this.gs.getDistrictList(),
@@ -194,22 +201,49 @@ public class SimulatedAnnealing implements Runnable{
 	        return fairness;
 	    }
 		
-//	    private void sendMove(Move move){
-//	        try {
-//	            session.getBasicRemote().sendText(move.toString());
-//	        } catch (IOException e) {
-//	            e.printStackTrace();
-//	        }
-//	    }
+	    private void sendMove(Move move){
+	        try {
+	            session.getBasicRemote().sendText(move.toString());
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
 	
-		public void setupSA(){	
-			for(Entry<Integer, List<Precinct>> entry : gs.getPrecinctData().entrySet()) {
-				List<Precinct> currPrecs = entry.getValue();
-				for(Precinct p: currPrecs) {
-					allPrecincts.add(p);
-				}
+	public void setupSA(StateID stateID){	
+		for(Entry<Integer, List<Precinct>> entry : gs.getPrecinctData().entrySet()) {
+			List<Precinct> currPrecs = entry.getValue();
+			for(Precinct p: currPrecs) {
+				allPrecincts.add(p);
 			}
 		}
+		String state = "";
+		if(StateID.NE == stateID){
+			state = "Nebraska";
+		}
+		else if(StateID.KS == stateID){
+			state = "Kansas";
+		}
+		else if(StateID.IA == stateID){
+			state = "Iowa";
+		}
+		byte[] mapData = null;
+		
+		try {
+			mapData = Files.readAllBytes(Paths.get("c://users//c/workspace//CSE308//src//" + state + ".json"));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		neighbors = new HashMap<>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		try {
+			neighbors = objectMapper.readValue(mapData, HashMap.class);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	public List<String> getMoves(){
 		return moves;
@@ -306,52 +340,53 @@ public Precinct selectBoundaryPrecinct(District d, List<Precinct> districtPrecin
 		return null;
     }
     
-    public List<Precinct> getNeighbors(Precinct boundaryPrecinct, List<Precinct> allPrecincts){
+    public List<Integer> getNeighbors(Precinct boundaryPrecinct, List<Precinct> allPrecincts){
     	
-		String bp = null, pb = null;
-		String bps = null, pbs = null;
-		JSONObject bpobj = null, pbobj = null;
-		JSONObject gobj = null;
-		Geometry bpi = null , pbi = null; 
-		GeoJsonReader reader = new GeoJsonReader();
-/****** Boundary Precinct Geometry ******/		
-		bp = boundaryPrecinct.getBoundaryJSON();
-		try {
-			bpobj = new JSONObject(bp);
-			gobj = bpobj.getJSONObject("geometry");
-			bps = gobj.toString();
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-		try {
-			bpi = reader.read(bps);
-			bpi = reducer.reduce(bpi).buffer(0);
-		} catch (ParseException e1) {
-			e1.printStackTrace();
-		}
-/******  Neighbor Precinct Geometry ******/
-        List<Precinct> realNeighborList = new ArrayList<Precinct>();  //List of neighbor precincts to be returned
-        for (Precinct p: allPrecincts) {
-			pb = p.getBoundaryJSON();	
-			try {
-				pbobj = new JSONObject(pb);
-				gobj = pbobj.getJSONObject("geometry");
-				pbs = gobj.toString();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			try {
-				 pbi = reader.read(pbs);
-				 pbi = reducer.reduce(pbi).buffer(0);
-//					System.out.println("printing pbi   "+ pbi.toString());
-			} catch (ParseException e) {
-				e.printStackTrace();
-			}
-			if(pbi.intersects(bpi)) {
-				if(boundaryPrecinct.getPrecinctId()!=p.getPrecinctId())
-				realNeighborList.add(p);
-			}
-		}
-        return realNeighborList;
+    	return neighbors.get("" + boundaryPrecinct.getPrecinctId());
+//		String bp = null, pb = null;
+//		String bps = null, pbs = null;
+//		JSONObject bpobj = null, pbobj = null;
+//		JSONObject gobj = null;
+//		Geometry bpi = null , pbi = null; 
+//		GeoJsonReader reader = new GeoJsonReader();
+///****** Boundary Precinct Geometry ******/		
+//		bp = boundaryPrecinct.getBoundaryJSON();
+//		try {
+//			bpobj = new JSONObject(bp);
+//			gobj = bpobj.getJSONObject("geometry");
+//			bps = gobj.toString();
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+//		try {
+//			bpi = reader.read(bps);
+//			bpi = reducer.reduce(bpi).buffer(0);
+//		} catch (ParseException e1) {
+//			e1.printStackTrace();
+//		}
+///******  Neighbor Precinct Geometry ******/
+//        List<Precinct> realNeighborList = new ArrayList<Precinct>();  //List of neighbor precincts to be returned
+//        for (Precinct p: allPrecincts) {
+//			pb = p.getBoundaryJSON();	
+//			try {
+//				pbobj = new JSONObject(pb);
+//				gobj = pbobj.getJSONObject("geometry");
+//				pbs = gobj.toString();
+//			} catch (JSONException e) {
+//				e.printStackTrace();
+//			}
+//			try {
+//				 pbi = reader.read(pbs);
+//				 pbi = reducer.reduce(pbi).buffer(0);
+////					System.out.println("printing pbi   "+ pbi.toString());
+//			} catch (ParseException e) {
+//				e.printStackTrace();
+//			}
+//			if(pbi.intersects(bpi)) {
+//				if(boundaryPrecinct.getPrecinctId()!=p.getPrecinctId())
+//				realNeighborList.add(p);
+//			}
+//		}
+//        return realNeighborList;
     }
 }
